63b12c2b406a35eccb444ab7eb1aacd697f3c1ae
olgakup <olga.kupchevskaya.812@my.csun.edu>
2016-11-30 22:54:45 -0800

    added edit personal fragment

--- app/src/main/java/com/csun_sunlink/csuncareercenter/Profile/ProfileEditPersonalFragment.java
+++ app/src/main/java/com/csun_sunlink/csuncareercenter/Profile/ProfileEditPersonalFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.csun_sunlink.csuncareercenter.R;
@@ -20,15 +21,18 @@ public class ProfileEditPersonalFragment extends Fragment {
     private Context ctx;
     protected UserPersonal currUser;
 
+    Button save;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragement_edit_profile, container, false);
         // Inflate the layout for this fragment
         ListView listView =(ListView) view.findViewById(R.id.edit_list);
         ctx = getActivity().getApplicationContext();
-
+        //save = (Button)findViewById(R.id.save_edit_button);
+        //set on click listener
         ProfileBgTask bgTask = new ProfileBgTask(ctx, listView);
-        bgTask.execute("edutPersonalFragment");
+        bgTask.execute("editPersonalFragment");
 
         return view;
     }
